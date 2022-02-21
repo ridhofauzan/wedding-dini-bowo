bfb9bd7c14ff8e872fbfd650070c98958b5406ea
Ridho Fauzan <ridhofauzan92@gmail.com>
2022-02-22 06:08:59 +0700

    Adding GMaps

--- src/pages/Maps.vue
+++ src/pages/Maps.vue
@@ -3,9 +3,20 @@
         <h1 class="db-title">
             Location
         </h1>
+        <div class="db-bride">
+            <GoogleMap />
+        </div>
     </div>
 </template>
 
+<script>
+import GoogleMap from './../components/GoogleMap.vue'
+export default {
+    components: {
+        GoogleMap
+    }
+}
+</script>
 <style>
 .db-wrapper {
     width: 100%;
@@ -21,5 +32,13 @@ h1.db-title {
     letter-spacing: 3px;
     text-align: center;
     font-family: 'Abril Fatface', cursive;
+}
+.db-bride {
+    border-radius: 4px;
+    margin: 8px auto;
+    width: calc(100% - 60px);
+    padding: 16px;
+    background: #e5e5e5;
+    display: block;
 }
 </style>
